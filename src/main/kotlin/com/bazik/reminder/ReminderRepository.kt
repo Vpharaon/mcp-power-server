8ea88b532fdaba8f4911354ab337d03bdedf3685
viacheslav <farionik@gmail.com>
2025-12-17 21:43:46 +0300

    поправил зависимость

--- src/main/kotlin/com/bazik/reminder/ReminderRepository.kt
+++ src/main/kotlin/com/bazik/reminder/ReminderRepository.kt
@@ -2,6 +2,7 @@ package com.bazik.reminder
 
 import com.bazik.reminder.models.*
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
